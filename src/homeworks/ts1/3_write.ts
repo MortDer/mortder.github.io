304264612ba4e4faa7a45668d62247de27549818
MortDer <mortder@hotmail.com>
2025-10-02 13:29:24 +0300

    feat: add task 3 types and functions

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -54,3 +54,65 @@
  * Принимает дату создания (строка)
  * */
 // export const createRandomOperation = (createdAt: string) => {};
+
+import { v4 as uuidv4 } from 'uuid';
+import { faker } from '@faker-js/faker';
+
+type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+type Operation = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost' | 'Profit';
+};
+
+export const createRandomProduct = (createdAt: string): Product => {
+  return {
+    id: uuidv4(),
+    name: faker.commerce.productName(),
+    photo: faker.image.url(),
+    desc: faker.commerce.productDescription(),
+    createdAt,
+    oldPrice: parseFloat(faker.commerce.price()),
+    price: parseFloat(faker.commerce.price()),
+    category: createRandomCategory(),
+  };
+};
+
+export const createRandomOperation = (createdAt: string): Operation => {
+  return {
+    id: uuidv4(),
+    name: faker.commerce.productName(),
+    desc: faker.commerce.productDescription(),
+    createdAt,
+    amount: Math.random() * 1000,
+    category: createRandomCategory(),
+    type: Math.random() > 0.5 ? 'Cost' : 'Profit',
+  };
+};
+
+export const createRandomCategory = (): Category => {
+  return {
+    id: uuidv4(),
+    name: faker.commerce.department(),
+  };
+};
